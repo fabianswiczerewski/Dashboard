a46dc99415880a306fc0092e274195e0ff660fcb
Fabian Świczerewski <88277810+fabianswiczerewski@users.noreply.github.com>
2022-04-12 23:25:25 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85361',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '15374',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'DJI XL Drone',
+    productNumber: '36636',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'GPS HD camera Drone',
+    productNumber: '73451',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'KR1707 Mini Drone',
+    productNumber: '23461',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+]
